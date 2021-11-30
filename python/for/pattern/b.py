ea3eb768e9cd827761db14831270142662d6ad17
Chandan Thakur <contact.shubhamthakur@gmail.com>
2021-11-30 18:40:05 +0530

    for in python

---
+++ python/for/pattern/b.py
@@ -0,0 +1,6 @@
+i=4
+while i>=0:
+    for j in range(0,i+1):
+        print("*",end="")
+    print("")    
+    i=i-1    
---
+++ python/for/pattern/c.py
@@ -0,0 +1,10 @@
+l=0
+i=6
+while i>=0:
+    for k in range(0,l+1):
+        print(" ",end="")
+    for j in range(0,i+1):
+        print("*",end="")
+    l=l+1
+    print("")
+    i=i-2     
